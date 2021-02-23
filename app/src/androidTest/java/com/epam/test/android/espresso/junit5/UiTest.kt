038f4db8550a8df07241f4b6539640db35eec846
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-02-23 16:40:34 +0300

    Corrected test annotation

--- app/src/androidTest/java/com/epam/test/android/espresso/junit5/UiTest.kt
+++ app/src/androidTest/java/com/epam/test/android/espresso/junit5/UiTest.kt
@@ -9,7 +9,7 @@ import com.epam.test.R
 import com.epam.test.ui.login.LoginActivity
 import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.hamcrest.Matchers
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.RegisterExtension
 
